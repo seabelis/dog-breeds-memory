b56c2f2e76d74591b52b08c76c516dd581024c5a
seabelis <seabelis74@gmail.com>
2019-10-11 13:34:44 +0200

    changed name of action for clairty

---
+++ src/actions/imgRandomizer.js
@@ -0,0 +1,8 @@
+//I'm not sure yet if I need this.
+export const IMG_RANDOMIZER = 'IMG_RANDOMIZER'
+
+export const imgRandomizer = (image) => ({
+    type: IMG_RANDOMIZER,
+    payload: image
+  
+})
